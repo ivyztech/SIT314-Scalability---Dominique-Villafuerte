bdf551df769c210d6ca87ba55447fac759245e69
ivyztech <108420656+ivyztech@users.noreply.github.com>
2025-07-20 12:53:12 +1000

    new warning_request.js

---
+++ sit314_week2/warning_request.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const host = "127.0.0.1";
+const port = 6000;
+const region = "Central";
+
+const client = net.createConnection(port, host, () => {
+    console.log("Connected");
+
+    
+    setInterval(() => {
+        client.write(`request,0,${region}`);
+    }, 2000);
+});
+
+client.on("data", (data) => {
+    console.log(`Received: ${data}`);
+});
+
+client.on("error", (error) => {
+    console.log(`Error: ${error.message}`);
+});
+
+client.on("close", () => {
+    console.log("Connection closed");
+});
